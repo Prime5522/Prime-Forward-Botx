8fb91ef7886a0619afecd084a6470e686461cdf2
PRIME BOT'z <sahidhasannayem@gmail.com>
2024-11-22 08:24:57 +0600

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ class Config:
     API_ID = environ.get("API_ID", "")
     API_HASH = environ.get("API_HASH", "")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = environ.get("BOT_SESSION", "Forward-Bot") 
+    BOT_SESSION = environ.get("BOT_SESSION", "Prime-Forward-Botx") 
     DATABASE_URI = environ.get("DATABASE", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") 
